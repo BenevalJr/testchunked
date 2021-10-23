ac8a6016d7c2d1ed6bef4bae4366bdd389a21110
Beneval Junior <benevaljr@gmail.com>
2021-10-23 00:06:45 -0300

    correct convert id to string

--- pages/api/v1/testchunked.js
+++ pages/api/v1/testchunked.js
@@ -1,10 +1,11 @@
 async function TestChunked(aRequest, aResponse) {
   var lResponse = [];
 
-  for (var i = 0; i < 10000; i++) {
+  for (var i = 0; i < 100000; i++) {
+    //let lId = i.toString();
     lResponse.push({
       id: i.toString,
-      description: "description " + i.toString,
+      description: "description " + i.toString(),
     });
   }
 
