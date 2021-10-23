e7ec73ef3b10ddf89e1091d70120009f5be55fa8
Beneval Junior <benevaljr@gmail.com>
2021-10-23 00:01:42 -0300

    add json array

--- pages/api/v1/testchunked.js
+++ pages/api/v1/testchunked.js
@@ -1,5 +1,13 @@
 async function TestChunked(aRequest, aResponse) {
-  var lResponse = { status: "Ok" };
+  var lResponse = [];
+
+  for (var i = 0; i < 10000; i++) {
+    lResponse.push({
+      id: i.toString,
+      description: "description " + i.toString,
+    });
+  }
+
   aResponse.status(200).json(lResponse);
 }
 
